7f0e11a92d4f372f398018765734ebf643078410
ilona <gogiashvili.ilona@gmail.com>
2024-03-14 20:43:26 -0400

    Inital commit

---
+++ cash-in-crunch/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Welcome!</h1>
+      <h2>for now there is singup/login only </h2>
+    </div>
+  );
+};
+
+export default Dashboard;
