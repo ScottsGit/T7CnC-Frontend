391de179d068a2546c161576fbacce50f9a96c55
Yuan <whataever0317@gmail.com>
2024-03-15 20:32:31 -0400

    init landing page

--- cash-in-crunch/src/App.js
+++ cash-in-crunch/src/App.js
@@ -13,6 +13,25 @@ import { UserProvider, UserContext } from "./context/UserContext";
 import Dashboard from "./components/Dashboard";
 import Footer from "./components/Footer";
 
+
+import { ThemeProvider } from '@mui/material/styles';
+import { createTheme } from '@mui/material/styles';
+import { Landing } from "./pages/landingPage";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#32004C', // Change primary color
+    },
+    secondary: {
+      main: '#C0FFD0', // Change secondary color
+    },
+    // Add more custom colors if needed
+  },
+});
+
+
+
 function App() {
   const { loggedIn } = useContext(UserContext);
   const [userData, setUserData] = useState([]);
@@ -31,38 +50,42 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <NavBar />
-      <ul>
-        {userData.map((user) => (
-          <li key={user.id}>{user.username}</li>
-        ))}
-      </ul>
-      <UserProvider>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route exact path="/" element={<Dashboard />} />
-          <Route
-            path="/profile"
-            element={loggedIn ? <Profile /> : <Navigate to="/login" />}
-          />
-          <Route
-            path="/accounts"
-            element={loggedIn ? <Accounts /> : <Navigate to="/login" />}
-          />
-          <Route
-            path="/spendings"
-            element={loggedIn ? <Spending /> : <Navigate to="/login" />}
-          />
-          <Route
-            path="/goals"
-            element={loggedIn ? <Goals /> : <Navigate to="/login" />}
-          />
-        </Routes>
-        <Footer />
-      </UserProvider>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        {/* <NavBar />
+        <ul>
+          {userData.map((user) => (
+            <li key={user.id}>{user.username}</li>
+          ))}
+        </ul> */}
+        <UserProvider>
+          <Routes>
+            <Route exact path="/" element={<Landing />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            {/* <Route exact path="/dashboard" element={<Dashboard />}> 
+              <Route
+                path="/profile"
+                element={loggedIn ? <Profile /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/accounts"
+                element={loggedIn ? <Accounts /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/spendings"
+                element={loggedIn ? <Spending /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/goals"
+                element={loggedIn ? <Goals /> : <Navigate to="/login" />}
+              />
+            </Route> */}
+          </Routes>
+          {/* <Footer /> */}
+        </UserProvider>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- cash-in-crunch/src/index.js
+++ cash-in-crunch/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import reportWebVitals from "./reportWebVitals";
 import { UserProvider } from "./context/UserContext";
 
 createRoot(document.getElementById("root")).render(
@@ -13,4 +12,4 @@ createRoot(document.getElementById("root")).render(
   </BrowserRouter>
 );
 
-reportWebVitals();
+
