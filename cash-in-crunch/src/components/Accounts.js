db93e0e9998a972f7b57c683c2cf7e1bdf41a0b9
Yuan <whataever0317@gmail.com>
2024-03-25 14:02:53 -0400

    fix folders

--- cash-in-crunch/src/components/Accounts.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Accounts = () => {
-  return <div></div>;
-};
-
-export default Accounts;
--- cash-in-crunch/src/components/Spending.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Spending = () => {
-  return <div></div>;
-};
-
-export default Spending;
